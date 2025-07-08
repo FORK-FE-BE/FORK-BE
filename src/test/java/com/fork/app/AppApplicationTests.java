ba6b5163885b360875b14f25e77fd43e1929e092
shrimpToWhale <2171180@hansung.ac.kr>
2025-07-08 19:16:37 +0900

    Normalize line endings

---
+++ src/test/java/com/fork/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fork.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
