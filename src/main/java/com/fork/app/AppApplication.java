151e7b464ffd11f1706ae2e906bf38516d35174a
juhyun <hanaheart030@gmail.com>
2025-08-12 04:32:48 +0900

    배포 모드에서 OpenAI 자동설정 제외

--- src/main/java/com/fork/app/AppApplication.java
+++ src/main/java/com/fork/app/AppApplication.java
@@ -3,11 +3,22 @@ package com.fork.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class AppApplication {
+//@SpringBootApplication
+//public class AppApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(AppApplication.class, args);
+//    }
+//
+//}
 
+@SpringBootApplication(
+        exclude = {
+                org.springframework.ai.model.openai.autoconfigure.OpenAiAudioSpeechAutoConfiguration.class
+        }
+)
+public class AppApplication {
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
     }
-
 }
