51ed2535f376f3aa95a3f3cb66fb27f2a9f11cc2
juhyun <hanaheart030@gmail.com>
2025-08-12 19:46:33 +0900

    Add application.yml for deployment

--- src/main/java/com/fork/app/AppApplication.java
+++ src/main/java/com/fork/app/AppApplication.java
@@ -12,18 +12,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //
 //}
 
-@SpringBootApplication(
-        exclude = {
-                org.springframework.ai.model.openai.autoconfigure.OpenAiAudioTranscriptionAutoConfiguration.class,
-                org.springframework.ai.model.openai.autoconfigure.OpenAiAudioSpeechAutoConfiguration.class,
-                org.springframework.ai.model.openai.autoconfigure.OpenAiChatAutoConfiguration.class,
-                org.springframework.ai.model.openai.autoconfigure.OpenAiEmbeddingAutoConfiguration.class,
-                org.springframework.ai.model.openai.autoconfigure.OpenAiImageAutoConfiguration.class,
-                org.springframework.ai.model.openai.autoconfigure.OpenAiModerationAutoConfiguration.class // 새로 추가
-                //org.springframework.ai.autoconfigure.vectorstore.redis.RedisVectorStoreAutoConfiguration.class, // 혹시 Redis VectorStore 연동 방지
-                //org.springframework.ai.autoconfigure.vectorstore.pgvector.PgVectorStoreAutoConfiguration.class // 혹시 pgvector 연동 방지
-        }
-)
+@SpringBootApplication(exclude = {
+        org.springframework.ai.model.openai.autoconfigure.OpenAiChatAutoConfiguration.class,
+        org.springframework.ai.model.openai.autoconfigure.OpenAiEmbeddingAutoConfiguration.class,
+        org.springframework.ai.model.openai.autoconfigure.OpenAiImageAutoConfiguration.class,
+        org.springframework.ai.model.openai.autoconfigure.OpenAiAudioSpeechAutoConfiguration.class,
+        org.springframework.ai.model.openai.autoconfigure.OpenAiModerationAutoConfiguration.class
+})
 public class AppApplication {
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
